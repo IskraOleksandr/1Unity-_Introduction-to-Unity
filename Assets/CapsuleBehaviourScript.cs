3bfac4d1e9bcab6eb6ee9efedccaaa3995f9ded6
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-03-31 16:02:52 +0300

    Добавьте файлы проекта.

---
+++ Assets/CapsuleBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private float x_rotate_speed = 1.0F;
+	[SerializeField]
+	private float y_rotate_speed = 0.5F;
+	[SerializeField]
+	private float z_rotate_speed = 0.8F;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(x_rotate_speed, y_rotate_speed, z_rotate_speed);
+		
+    }
+}
